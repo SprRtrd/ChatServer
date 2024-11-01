fdb57de1a9b54971bb62ce51880dbb06bc1d899b
olpemila <olli.p.m.laiho@student.jyu.fi>
2024-11-01 11:16:00 +0200

    alun kokeiluja

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.ComponentModel;
+using System.Globalization;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -10,7 +12,16 @@ namespace ChatServer
     {
         static void Main(string[] args)
         {
-            
+            string[] osoite = ["127.0.0.1", "8080"];
+            HttpListener listener = new HttpListener();
+
+            foreach(string numero in osoite){
+                listener.Prefixes.Add(numero);
+            }
+
+            listener.Start();
+            Console.WriteLine("Listening...");
+
         }
     }
 }

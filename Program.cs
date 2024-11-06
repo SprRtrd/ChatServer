770c13115a02a464adbe0e3ae6fd7ca93ca18e83
olpemila <olli.p.m.laiho@student.jyu.fi>
2024-11-06 18:38:42 +0200

    WebSocketin testailua

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
 using System.Data.SQLite;
+using System.Net.WebSockets;
 
 
 namespace ChatServer
@@ -18,10 +19,11 @@ namespace ChatServer
             //TODO: Viestien tallennus tietokantaan. Käyttäjätunnusten luominen. Viestien esittäminen clientille
             DatabaseHandler dbHandler = new DatabaseHandler();
             dbHandler.CreateDatabase();
+            
 
 
             string[] osoite = ["http://127.0.0.1:8080/"];
-            HttpListener listener = new HttpListener();
+            HttpListener listener = new();
 
             foreach(string numero in osoite){
                 listener.Prefixes.Add(numero);
@@ -29,7 +31,13 @@ namespace ChatServer
 
             listener.Start();
             Console.WriteLine("Listening...");
-            HttpListenerContext context = listener.GetContext();
+            
+            YhteydenTestaus(listener);
+          
+            
+            
+            
+            /*HttpListenerContext context = listener.GetContext();
             HttpListenerRequest request = context.Request;
 
             Stream body = request.InputStream;
@@ -44,16 +52,51 @@ namespace ChatServer
             response.ContentLength64 = buffer.Length;
             Stream output = response.OutputStream;
             output.Write(buffer,0,buffer.Length);
-            output.Close();
+            output.Close();*/
            
             
             
             
             
             
-            Console.WriteLine("Lol {0}", s);
+
             Console.ReadLine();           
 
+        }
+
+        static async void YhteydenTestaus(HttpListener listener){
+            while (true)
+            {
+                HttpListenerContext context = await listener.GetContextAsync();
+                if (context.Request.IsWebSocketRequest){
+                WebSocketContext webSocketContext = await context.AcceptWebSocketAsync(subProtocol: null);
+                WebSocket webSocket = webSocketContext.WebSocket;
+
+                Console.WriteLine("Client connected");
+
+                byte[] receiveBuffer = new byte[1024];
+                if (webSocket.State == WebSocketState.Open){
+                    WebSocketReceiveResult receiveResult = await webSocket.ReceiveAsync(new ArraySegment<byte>(receiveBuffer), CancellationToken.None);
+                    if (receiveResult.MessageType == WebSocketMessageType.Text){
+                        string receivedMessage = Encoding.UTF8.GetString(receiveBuffer, 0, receiveResult.Count);
+                        Console.WriteLine($"Received ,essage: {receivedMessage}");
+
+                        byte[] buffer = Encoding.UTF8.GetBytes("Hello from server");
+                        await webSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+                    }
+                    else if (receiveResult.MessageType == WebSocketMessageType.Close){
+                        await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+                        Console.WriteLine("WebSocket closed");
+                    }
+                }
+              }
+              else{
+                context.Response.StatusCode = 400;
+                context.Response.Close();
+              
+        
+                }
+            }
         }
     }
 }
